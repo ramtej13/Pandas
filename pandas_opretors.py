46d15afb5c50839720d6ef721d17ad69d3169db8
ramtej13 <ramtej.j13@gmail.com>
2020-09-01 02:48:52 +0530

    first test

---
+++ pandas_opretors.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+db_code = pd.DataFrame({"k1":["A","A","B","B","C","C"],
+                        "col1":[100,200,200,300,300,500],
+                        "col2":["lol1","lol0","lol2","lol3","lol4","lol6"]})
+print(db_code)
+
